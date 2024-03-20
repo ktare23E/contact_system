c7ae144f31a71c1a86a9e973dffa3f09336f718f
Kristian Kharl Tare <kristiankharl.tare@nmsc.edu.ph>
2024-03-20 20:02:05 +0800

    experimenting the display and search number of contact display

--- controllers/user_login.php
+++ controllers/user_login.php
@@ -10,9 +10,6 @@
         $query = "SELECT * FROM users WHERE email = '$email'";
         $runQuery = mysqli_query($conn,$query);
 
-        //retrieve admin
-        $adminQuery = "SELECT * FROM admin WHERE admin_email = '$email'";
-        $runAdminQuery = mysqli_query($conn,$adminQuery);
 
         if(mysqli_num_rows($runQuery) > 0 ){
             $row = mysqli_fetch_assoc($runQuery);
@@ -23,19 +20,6 @@
                 $_SESSION['is_logged_in'] = 1;
                 $_SESSION['user_id'] = $row['user_id'];
                 header('location: ../users/index.php');
-            }else{
-                header('location: ../index.php?prompt=Wrong password');
-            }
-
-    
-        } elseif(mysqli_num_rows($runAdminQuery) > 0){
-            $adminRow = mysqli_fetch_assoc($runAdminQuery);
-            $adminHashPassword = $adminRow['admin_password'];
-
-            if(password_verify($password,$adminHashPassword)){
-                $_SESSION['isAdmin'] = 1;
-                $_SESSION['admin_id'] = $adminRow['admin_id'];
-                header('location: ../admin/index.php');
             }else{
                 header('location: ../index.php?prompt=Wrong password');
             }
